bb3640fec118a181ce784bcaf4822504965f9756
David Bezprozvani <david.bezprozvani@gmail.com>
2021-02-06 22:55:06 +0200

    Added i18next

---
+++ src/components/Header/LangSwitcher.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import { useTranslation } from "react-i18next";
+
+
+const LangSwitcher = () => {
+
+    const { i18n } = useTranslation();
+
+    const handleLangChange = (e) => {
+        i18n.changeLanguage(e.target.value)
+    }
+
+    return (
+        <Select
+            labelId="lang-label"
+            value={i18n.language}
+            onChange={handleLangChange}
+        >
+            <MenuItem value={'lt'}>LT</MenuItem>
+            <MenuItem value={'en'}>EN</MenuItem>
+        </Select>
+    )
+
+}
+
+export default LangSwitcher
